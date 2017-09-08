8d8fdb48163a5e1784293c06387d2e3e9c5215f4
flarefansy <31630361+flarefansy@users.noreply.github.com>
2017-09-07 20:06:23 -0500

    Add files via upload

---
+++ homework1_task3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  5 00:00:42 2017
+
+@author: Spencersun
+"""
+
+for i in range(700,1700):
+    if i%5 == 0 and i%2 == 0:
+        print (i)
+        
+
+    
+    
