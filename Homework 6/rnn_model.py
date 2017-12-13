9970e7ddf59ae23c38e721a3af6b8fb6fadb7453
flarefansy <31630361+flarefansy@users.noreply.github.com>
2017-12-12 20:40:38 -0600

    Add files via upload

---
+++ Homework 6/rnn_model.py
@@ -0,0 +1,74 @@
+
+
+import tensorflow as tf
+
+class TRNNConfig(object):
+    embedding_dim = 64      
+    seq_length = 600        
+    num_classes = 10        
+    vocab_size = 5000      
+
+    num_layers= 1           
+    hidden_dim = 64       
+    rnn = 'basic'           
+
+    dropout_keep_prob = 0.2 
+    learning_rate = 1e-3    
+
+    batch_size = 128         
+    num_epochs = 10          
+
+    print_per_batch = 100    
+    save_per_batch = 10   
+
+
+class TextRNN(object):
+    def __init__(self, config):
+        self.config = config
+
+        self.input_x = tf.placeholder(tf.int32, [None, self.config.seq_length], name='input_x')
+        self.input_y = tf.placeholder(tf.float32, [None, self.config.num_classes], name='input_y')
+        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+
+        self.rnn()
+
+    def rnn(self):
+        def lstm_cell():   
+            return tf.contrib.rnn.BasicLSTMCell(self.config.hidden_dim, state_is_tuple=True)
+
+        def basic_cell():  
+            return tf.contrib.rnn.BasicRNNCell(self.config.hidden_dim)
+
+        def dropout():
+            if (self.config.rnn == 'lstm'):
+                cell = lstm_cell()
+            else:
+                cell = basic_cell()
+            return tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=self.keep_prob)
+
+        with tf.device('/cpu:0'):
+            embedding = tf.get_variable('embedding', [self.config.vocab_size, self.config.embedding_dim])
+            embedding_inputs = tf.nn.embedding_lookup(embedding, self.input_x)
+
+        with tf.name_scope("rnn"):
+            cells = [dropout() for _ in range(self.config.num_layers)]
+            rnn_cell = tf.contrib.rnn.MultiRNNCell(cells, state_is_tuple=True)
+
+            _outputs, _ = tf.nn.dynamic_rnn(cell=rnn_cell, inputs=embedding_inputs, dtype=tf.float32)
+            last = _outputs[:, -1, :]  
+
+        with tf.name_scope("score"):
+            fc = tf.layers.dense(last, self.config.hidden_dim, name='fc1')
+            fc = tf.nn.relu(fc)
+
+            self.logits = tf.layers.dense(fc, self.config.num_classes, name='fc2')
+            self.y_pred_cls = tf.argmax(tf.nn.softmax(self.logits), 1)  
+
+        with tf.name_scope("optimize"):
+            cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.input_y)
+            self.loss = tf.reduce_mean(cross_entropy)
+            self.optim = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate).minimize(self.loss)
+
+        with tf.name_scope("accuracy"):
+            correct_pred = tf.equal(tf.argmax(self.input_y, 1), self.y_pred_cls)
+            self.acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
